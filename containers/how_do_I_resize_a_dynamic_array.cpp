7b920ab1d8250ea0b835121379adc62eb9c4dd37
Rex Kerr <rexkerr@gmail.com>
2013-05-15 08:37:31 -0600

    Added resize array example

---
+++ containers/how_do_I_resize_a_dynamic_array.cpp
@@ -0,0 +1,29 @@
+#include <vector>     // for vector
+#include <iostream>   // for cout
+#include <algorithm>  // for for_each
+
+namespace
+{
+   void print_vector(std::vector<int> v)
+   {
+      std::cout << "size:  " << v.size() << std::endl;
+      std::cout << "content:  ";
+      std::for_each(begin(v), end(v), [](int i){ std::cout << i << " " << std::endl; });
+      std::cout << "\n" << std::endl;
+   }
+}
+
+int main()
+{
+   std::vector<int> v = {1, 2, 3};
+   print_vector(v);
+
+   v.resize(5);
+   // Vector now contains: 1,2,3,0,0
+   print_vector(v);
+
+   v.resize(7, 42);
+   // Vector now contains: 1,2,3,0,0,42,42
+   print_vector(v);
+}
+
