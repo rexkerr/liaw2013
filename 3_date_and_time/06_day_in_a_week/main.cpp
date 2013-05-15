e4f06dc4c8088f8032f761050b14f1585f5e5be3
Rob Tsuk <robtsuk@google.com>
2013-05-14 21:21:22 -0600

    Add in boost

--- 3_date_and_time/06_day_in_a_week/main.cpp
+++ 3_date_and_time/06_day_in_a_week/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+#include "boost/date_time/gregorian/gregorian.hpp"
+
 using namespace std;
+using namespace boost::gregorian;
 
 int main(int argc, char** argv)
 {
-    cout << "Hello" << endl;
+    date cppNow(2013, May, 12);
+    
+    cout << "Date to test is "<< cppNow << "." << endl;
+    cout << "Day of year: " << cppNow.day_of_year() << endl;
+    cout << "ISO week number: " << cppNow.week_number() << endl;
+    cout << "Day of week: " << cppNow.day_of_week() << endl;
 }
